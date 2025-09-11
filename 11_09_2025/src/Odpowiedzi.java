049cdb35565c158807f6bd5c64af2dd1f598d622
pilif139 <pifilif@gmail.com>
2025-09-11 13:59:47 +0200

    zadanie 4.2

--- 11_09_2025/src/Odpowiedzi.java
+++ 11_09_2025/src/Odpowiedzi.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Odpowiedzi {
@@ -21,8 +22,8 @@ public class Odpowiedzi {
     }
 
     private void WriteAnswer(String answer){
-        try (FileWriter fw = new FileWriter("wyniki4.txt")) {
-            fw.write(answer);
+        try (FileWriter fw = new FileWriter("wyniki4.txt", true)) {
+            fw.write(answer+"\n");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -42,6 +43,49 @@ public class Odpowiedzi {
                 firstNumber = liczba;
             }
         }
-        WriteAnswer(String.format("%d %d", counter, firstNumber));
+        WriteAnswer(String.format("1. %d %d", counter, firstNumber));
+    }
+
+    private ArrayList<Integer> rozkladNaCzynnikiPierwsze(int number){
+        ArrayList<Integer> rozkladNaCzynnikiPierwsze = new ArrayList<>();
+        if(number < 1){
+            return rozkladNaCzynnikiPierwsze;
+        }
+        while(number % 2 == 0){
+            rozkladNaCzynnikiPierwsze.add(2);
+            number /=2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            while (number % i == 0) {
+                rozkladNaCzynnikiPierwsze.add(i);
+                number /= i;
+            }
+        }
+
+        if(number > 2){
+            rozkladNaCzynnikiPierwsze.add(number);
+        }
+        return rozkladNaCzynnikiPierwsze;
+    }
+
+    public void Zadanie2(){
+        int najwiecejCzynnikow = 0;
+        Integer najwiecejCzynnikowLiczba = null;
+        int najwiecejRoznychCzynnikow = 0;
+        Integer najwiecejRoznychCzynnikowLiczba = null;
+        for(Integer liczba : liczby){
+            ArrayList<Integer> rozklad = rozkladNaCzynnikiPierwsze(liczba);
+            if(rozklad.size() > najwiecejCzynnikow){
+                najwiecejCzynnikow = rozklad.size();
+                najwiecejCzynnikowLiczba = liczba;
+                HashSet<Integer> rozneCzynniki = new HashSet<>(rozklad);
+                if(rozneCzynniki.size() > najwiecejRoznychCzynnikow){
+                    najwiecejRoznychCzynnikow = rozneCzynniki.size();
+                    najwiecejRoznychCzynnikowLiczba = liczba;
+                }
+            }
+        }
+        WriteAnswer(String.format("2. %d %d %d %d", najwiecejCzynnikowLiczba, najwiecejCzynnikow, najwiecejRoznychCzynnikowLiczba, najwiecejRoznychCzynnikow));
     }
 }
